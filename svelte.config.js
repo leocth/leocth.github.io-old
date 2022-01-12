ce6dce374a118160c2054174866870acc1fa5cad
leocth <22406910+leocth@users.noreply.github.com>
2022-01-13 00:20:34 +0800

    chore: configure for GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -9,6 +11,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/leocth.github.io',
+		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
