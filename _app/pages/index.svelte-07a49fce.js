e6c13295dfbebc82e6469e959d37afeb6b0ad101
leocth <22406910+leocth@users.noreply.github.com>
2022-01-13 00:27:56 +0800

    Updates

---
+++ _app/pages/index.svelte-07a49fce.js
@@ -0,0 +1,39 @@
+import{S as Ie,i as ke,s as we,w as _e,k as F,e as f,t as n,x as ye,m as L,c as h,a as u,h as o,d as r,b as $,y as ge,g as p,J as t,q as be,o as $e,B as Ee}from"../chunks/vendor-18182ef8.js";import{H as Me}from"../chunks/header-ce60fe24.js";import{L as xe}from"../chunks/_links-778b0753.js";function Ce(H){let s,b,c,d,y;return d=new xe({}),{c(){s=f("h1"),b=n("leocth"),c=F(),_e(d.$$.fragment)},l(a){s=h(a,"H1",{});var v=u(s);b=o(v,"leocth"),v.forEach(r),c=L(a),ye(d.$$.fragment,a)},m(a,v){p(a,s,v),t(s,b),p(a,c,v),ge(d,a,v),y=!0},i(a){y||(be(d.$$.fragment,a),y=!0)},o(a){$e(d.$$.fragment,a),y=!1},d(a){a&&r(s),a&&r(c),Ee(d,a)}}}function Ae(H){let s,b,c,d,y,a,v,E,J,R,I,G,Q,P,_,U,k,D,K,w,N,O,T,i,V,M,W,X,S,Y,Z,x,ee,te,C,re,ae,A,ne,oe,q,j,se,B;return s=new Me({props:{$$slots:{default:[Ce]},$$scope:{ctx:H}}}),{c(){_e(s.$$.fragment),b=F(),c=f("p"),d=n(`Just a guy on the internet. Student, zoomer, self-educated lazy programmer, procrastinator
+	extraordinaire. Call me Leo.`),y=F(),a=f("p"),v=n(`My preferred pronouns are
+	`),E=f("a"),J=n("he/him"),R=n(`, or
+	`),I=f("a"),G=n("\u4ED6/\u4F60"),Q=n(`
+	in Chinese.`),P=F(),_=f("p"),U=n(`I like programs that are easy to understand for humans, fast to execute for computers, safe to run
+	for years and decades to come. It is for this reason that I am a huge
+	`),k=f("a"),D=n("Rustacean"),K=n(". ("),w=f("a"),N=n("Ferris"),O=n(` would
+	be proud of me).
+	
+	published online for everyone to watch.`),T=F(),i=f("p"),V=n(`I like
+	`),M=f("a"),W=n("Minecraft"),X=n(`. I've created a number of Minecraft mods in the
+	past, but recently I have run out of attention and energy to maintain them and perfect them
+	further \u2013 Minecraft is such a
+	`),S=f("em"),Y=n("horrible"),Z=n(` platform to develop on, that neither mature toolchains like
+	`),x=f("a"),ee=n("Forge"),te=n(` nor more innovative ones like
+	`),C=f("a"),re=n("Fabric"),ae=n(` or
+	`),A=f("a"),ne=n("Quilt"),oe=n(`
+	could save this dumpster fire. Let's just hope they'll get better; Mojang's beyond saving.`),q=F(),j=f("p"),se=n(`I speak English and Mandarin, and write in English and (Simplified) Chinese. I'm also learning
+	German. The language toggle switch is in the bottom-right corner, you're welcome. My timezone's in
+	UTC+8, so you could probably figure out where I live now, lol.`),this.h()},l(e){ye(s.$$.fragment,e),b=L(e),c=h(e,"P",{});var l=u(c);d=o(l,`Just a guy on the internet. Student, zoomer, self-educated lazy programmer, procrastinator
+	extraordinaire. Call me Leo.`),l.forEach(r),y=L(e),a=h(e,"P",{});var g=u(a);v=o(g,`My preferred pronouns are
+	`),E=h(g,"A",{href:!0});var le=u(E);J=o(le,"he/him"),le.forEach(r),R=o(g,`, or
+	`),I=h(g,"A",{href:!0});var ie=u(I);G=o(ie,"\u4ED6/\u4F60"),ie.forEach(r),Q=o(g,`
+	in Chinese.`),g.forEach(r),P=L(e),_=h(e,"P",{});var z=u(_);U=o(z,`I like programs that are easy to understand for humans, fast to execute for computers, safe to run
+	for years and decades to come. It is for this reason that I am a huge
+	`),k=h(z,"A",{href:!0});var fe=u(k);D=o(fe,"Rustacean"),fe.forEach(r),K=o(z,". ("),w=h(z,"A",{href:!0});var he=u(w);N=o(he,"Ferris"),he.forEach(r),O=o(z,` would
+	be proud of me).
+	
+	published online for everyone to watch.`),z.forEach(r),T=L(e),i=h(e,"P",{});var m=u(i);V=o(m,`I like
+	`),M=h(m,"A",{href:!0});var ue=u(M);W=o(ue,"Minecraft"),ue.forEach(r),X=o(m,`. I've created a number of Minecraft mods in the
+	past, but recently I have run out of attention and energy to maintain them and perfect them
+	further \u2013 Minecraft is such a
+	`),S=h(m,"EM",{});var me=u(S);Y=o(me,"horrible"),me.forEach(r),Z=o(m,` platform to develop on, that neither mature toolchains like
+	`),x=h(m,"A",{href:!0});var pe=u(x);ee=o(pe,"Forge"),pe.forEach(r),te=o(m,` nor more innovative ones like
+	`),C=h(m,"A",{href:!0});var ce=u(C);re=o(ce,"Fabric"),ce.forEach(r),ae=o(m,` or
+	`),A=h(m,"A",{href:!0});var de=u(A);ne=o(de,"Quilt"),de.forEach(r),oe=o(m,`
+	could save this dumpster fire. Let's just hope they'll get better; Mojang's beyond saving.`),m.forEach(r),q=L(e),j=h(e,"P",{});var ve=u(j);se=o(ve,`I speak English and Mandarin, and write in English and (Simplified) Chinese. I'm also learning
+	German. The language toggle switch is in the bottom-right corner, you're welcome. My timezone's in
+	UTC+8, so you could probably figure out where I live now, lol.`),ve.forEach(r),this.h()},h(){$(E,"href","https://en.pronouns.page/he"),$(I,"href","https://zh.pronouns.page/%E4%BB%96"),$(k,"href","https://rust-lang.org/"),$(w,"href","https://rustacean.net"),$(M,"href","https://minecraft.net"),$(x,"href","https://minecraftforge.net"),$(C,"href","https://fabricmc.net"),$(A,"href","https://quiltmc.org")},m(e,l){ge(s,e,l),p(e,b,l),p(e,c,l),t(c,d),p(e,y,l),p(e,a,l),t(a,v),t(a,E),t(E,J),t(a,R),t(a,I),t(I,G),t(a,Q),p(e,P,l),p(e,_,l),t(_,U),t(_,k),t(k,D),t(_,K),t(_,w),t(w,N),t(_,O),p(e,T,l),p(e,i,l),t(i,V),t(i,M),t(M,W),t(i,X),t(i,S),t(S,Y),t(i,Z),t(i,x),t(x,ee),t(i,te),t(i,C),t(C,re),t(i,ae),t(i,A),t(A,ne),t(i,oe),p(e,q,l),p(e,j,l),t(j,se),B=!0},p(e,[l]){const g={};l&1&&(g.$$scope={dirty:l,ctx:e}),s.$set(g)},i(e){B||(be(s.$$.fragment,e),B=!0)},o(e){$e(s.$$.fragment,e),B=!1},d(e){Ee(s,e),e&&r(b),e&&r(c),e&&r(y),e&&r(a),e&&r(P),e&&r(_),e&&r(T),e&&r(i),e&&r(q),e&&r(j)}}}class Le extends Ie{constructor(s){super();ke(this,s,null,Ae,we,{})}}export{Le as default};
